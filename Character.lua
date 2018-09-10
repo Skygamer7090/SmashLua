fa9cea4f8311c4111c2e24f83e2202d9bfa1149e
Skygamer7090 <43148257+Skygamer7090@users.noreply.github.com>
2018-09-10 15:07:07 -0400

    First commit

---
+++ Character.lua
@@ -0,0 +1,16 @@
+local c = require("Class")
+local Character = c:derive("Character")
+-----------vfsvsf
+function Character:new()
+
+end
+
+function Character:draw()
+
+end
+
+function Character:update(dt)
+
+end
+
+return Character
